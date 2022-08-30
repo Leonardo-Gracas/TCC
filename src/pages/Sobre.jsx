a9b9b80457589bf64b4db4915577dbdfdb3d20f7
Leonardo Gracas <84471102+Leonardo-Gracas@users.noreply.github.com>
2022-08-29 22:01:55 -0300

    Add files via upload

---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ImgHTMLAttributes } from "react";
+
+import './Sobre.css'
+
+export default props => {
+
+    return (
+        <main>
+            <section className="first">
+                <strong>Sobre</strong>
+                <div className="subline"></div>
+                <p>Detalhes e propósito do projeto</p>
+            </section>
+        </main>
+    )
+}
