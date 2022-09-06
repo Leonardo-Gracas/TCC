dd6bfa65075636af34189c19b53a1cec9cfda8b1
Retr0 <84470488+FelipeC-retr0@users.noreply.github.com>
2022-09-05 21:30:25 -0300

    Add files via upload

---
+++ src/pages/Integrantes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './Pages.css'
+
+export default props => {
+
+    return (
+        <main>
+            <section className="first">
+                <strong>Integrantes</strong>
+                <p>Detalhes sobre os integrantes</p>
+            </section>
+        </main>
+    )
+}
